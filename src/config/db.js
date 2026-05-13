74d6e2294202d97fad28b13ffbd15ec8dcc3b3d2
Caloca99 <159637297+Caloca99@users.noreply.github.com>
2026-05-13 14:12:37 -0500

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,31 +1,16 @@
 const mysql = require("mysql2/promise");
 
-const connectionUrl = process.env.DATABASE_URL || process.env.MYSQL_URL;
-
-const baseConfig = connectionUrl
-  ? (() => {
-      const url = new URL(connectionUrl);
-      return {
-        host: url.hostname,
-        user: decodeURIComponent(url.username),
-        password: decodeURIComponent(url.password),
-        database: url.pathname.replace("/", ""),
-        port: Number(url.port || 3306),
-      };
-    })()
-  : {
-      host: process.env.DB_HOST || process.env.MYSQLHOST || "localhost",
-      user: process.env.DB_USER || process.env.MYSQLUSER || "root",
-      password: process.env.DB_PASSWORD || process.env.MYSQLPASSWORD || "",
-      database: process.env.DB_NAME || process.env.MYSQLDATABASE || "crud_profesores",
-      port: Number(process.env.DB_PORT || process.env.MYSQLPORT || 3306),
-    };
+const url = new URL(process.env.DATABASE_URL);
 
 const pool = mysql.createPool({
-  ...baseConfig,
+  host: url.hostname,
+  user: decodeURIComponent(url.username),
+  password: decodeURIComponent(url.password),
+  database: url.pathname.replace("/", ""),
+  port: Number(url.port),
   ssl: {
-  rejectUnauthorized: false,
-},
+    rejectUnauthorized: false,
+  },
   waitForConnections: true,
   connectionLimit: 10,
 });
